713953458a093c96bf964b4b4f48a0f58552f075
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2018-03-14 17:00:15 +0900

    moved to correct directory

---
+++ Maven/Maven_Checkstyle_Plugin/Maven_Checkstyle_Plugin/src/main/java/jsample/App.java
@@ -0,0 +1,9 @@
+package jsample;
+
+public class App {
+	public static void main(String[] args) {
+
+		String errorStr = "Hello !!!" + " This is string." + " It should give error " + " while using findbug";
+		System.out.println(errorStr);
+	}
+}
--- Maven_Checkstyle_Plugin/Maven_Checkstyle_Plugin/src/main/java/jsample/App.java
+++
@@ -1,9 +0,0 @@
-package jsample;
-
-public class App {
-	public static void main(String[] args) {
-
-		String errorStr = "Hello !!!" + " This is string." + " It should give error " + " while using findbug";
-		System.out.println(errorStr);
-	}
-}
